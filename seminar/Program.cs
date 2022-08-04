a9cc58d1b74ae74f02e1e3a26cb95fd115b7a239
Sergey Z <zassipkin.com>
2022-08-04 21:10:14 +0300

    Secret number game

---
+++ seminar/Program.cs
@@ -0,0 +1,34 @@
+﻿// int number1 = Convert.ToInt32(Console.ReadLine());
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(number1 + number2);
+
+int secretNumber = 7;
+int count = 3;
+bool flagWin = false;
+while(count > 0)
+{
+    Console.WriteLine("Type the number in the period 1 to 10");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+count = count - 1; // it's possible to use 'count--'
+if(userNumber > secretNumber)
+{
+    Console.WriteLine("Secret number is less");
+    Console.WriteLine(count + " is left");
+}
+else if (userNumber < secretNumber)
+{
+    Console.WriteLine("Secret number is bigger");
+    Console.WriteLine(count + " is left");
+}
+else
+{
+    Console.WriteLine("You win!");
+    flagWin = true;
+    break;
+}
+}
+if(!flagWin)
+{
+    Console.WriteLine("You loose. Secret number was " + secretNumber);
+}
